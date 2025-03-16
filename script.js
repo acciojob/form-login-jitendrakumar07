1481ce0c439d20390ff170327f0942029a698902
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-03-16 23:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+  
+  event.preventDefault(); 
 
+  
+  const firstName = document.forms["form1"]["fname"].value;
+  const lastName = document.forms["form1"]["lname"].value;
+  alert("First Name: " + firstName + "\nLast Name: " + lastName); 
+
+  
+  document.forms["form1"].reset(); 
 }
